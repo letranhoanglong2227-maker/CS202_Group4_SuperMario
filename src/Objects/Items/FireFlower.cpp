7cb50b21779d42ecd73ed0120017f99eefe8a779
Risa <dttoan2007@gmail.com>
2026-08-29 18:06:31 +0700

    fix(person3): Update FireFlower sprite coordinates to prevent flashing with Coin and Star

--- src/Objects/Items/FireFlower.cpp
+++ src/Objects/Items/FireFlower.cpp
@@ -15,10 +15,10 @@ FireFlower::FireFlower(const sf::Vector2f& pos, bool popped)
     entitySprite.setScale({2.0f, 2.0f});
     if (animationComponent) {
         animationComponent->addAnimation("idle", { 
-            sf::IntRect({53, 36}, {16, 16}),
-            sf::IntRect({70, 36}, {16, 16}),
-            sf::IntRect({87, 36}, {16, 16}),
-            sf::IntRect({104, 36}, {16, 16})
+            sf::IntRect({2, 36}, {16, 16}),
+            sf::IntRect({19, 36}, {16, 16}),
+            sf::IntRect({36, 36}, {16, 16}),
+            sf::IntRect({36, 36}, {16, 16}) // Duplicate last frame since 53,36 is a Star
         });
     }
 }
